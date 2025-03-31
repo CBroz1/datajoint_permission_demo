1aa89fbd451eb5e0c3ca24380beb368c5ab96cf7
CBroz1 <CBrozdowski@yahoo.com>
2025-03-31 14:00:25 -0500

    Add notes, simplify table

--- sql/2_declare.sql
+++ sql/2_declare.sql
@@ -5,21 +5,13 @@ USE common_one;
 
 CREATE TABLE IF NOT EXISTS `one` (
   a INT NOT NULL,
-  b INT NOT NULL,
-  PRIMARY KEY (a, b)
+  PRIMARY KEY (a)
 );
 
--- Create `common_two.two` table with a foreign key referencing `common_one.one`
-CREATE DATABASE IF NOT EXISTS common_two;
-
-USE common_two;
-
 CREATE TABLE IF NOT EXISTS `two` (
-  c INT NOT NULL,
-  d INT NOT NULL,
+  b INT NOT NULL,
   a INT,
-  b INT,
-  PRIMARY KEY (c, d),
-  FOREIGN KEY (a, b) REFERENCES common_one.one(a, b)
+  PRIMARY KEY (b),
+  FOREIGN KEY (a) REFERENCES common_one.one(a)
 );
 
